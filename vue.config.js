e68ec4962efbb669ebfda19bf8e9a145fdb29386
hcc <huangcheng3576@gmail.com>
2021-03-18 17:58:04 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     chainWebpack: config => {
-        console.log('config', config.plugin('html'))
         config.plugin('html')
             .tap(args => {
                 args[0].title = "my-test"
                 return args
             })
+        console.log('config', config.toString())
     },
 }
