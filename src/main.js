8fa606477ce6218caab6eb1a02f06b38b755777c
hcc <huangcheng3576@gmail.com>
2021-03-22 17:19:42 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,14 +6,3 @@ Vue.config.productionTip = false
 new Vue({
     render: h => h(App)
 }).$mount('#app')
-
-module.exports = {
-    chainWebpack: config => {
-        console.log('config', config.plugin('html'))
-        config.plugin('html')
-            .tap(args => {
-                args[0].title = "my-test"
-                return args
-            })
-    },
-}
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,33 @@
+// module.exports = {
+//     chainWebpack: config => {
+//         config.plugin('html')
+//             .tap(args => {
+//                 args[0].title = "my-test"
+//                 return args
+//             })
+//     },
+// }
+
+// 拼接路径
 module.exports = {
-    chainWebpack: config => {
+    outputDir: 'dist', // 默认dist
+    indexPath: 'index.html', // Default: 'index.html'
+    filenameHashing: true,
+    lintOnSave: false,
+    devServer: {
+        host: 'localhost',
+        port: '8087', //代理端口
+        https: false,
+        open: 'Google Chrome', //项目启动时是否自动打开浏览器，我这里设置为false,不打开，true表示打开
+        hotOnly: true,
+    },
+    chainWebpack: (config) => {
         config.plugin('html')
             .tap(args => {
                 args[0].title = "my-test"
                 return args
             })
-        console.log('config', config.toString())
-    },
+
+        // config.plugins.delete('preload')
+    }
 }
